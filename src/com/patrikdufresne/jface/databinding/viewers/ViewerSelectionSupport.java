2a80736abaa71a25bbc877ff46d277b5a47f5178
Patrik Dufresne <ikus060@gmail.com>
2013-02-28 10:59:28 -0500

    Ajout d'une fonction observeSingleSelectionWithFocus ;a ViewerSelectionSupport.

--- src/com/patrikdufresne/jface/databinding/viewers/ViewerSelectionSupport.java
+++ src/com/patrikdufresne/jface/databinding/viewers/ViewerSelectionSupport.java
@@ -5,6 +5,7 @@
 package com.patrikdufresne.jface.databinding.viewers;
 
 import org.eclipse.core.databinding.observable.list.IObservableList;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.core.databinding.observable.value.WritableValue;
 import org.eclipse.jface.databinding.viewers.ViewerProperties;
 import org.eclipse.jface.viewers.TableViewer;
@@ -22,9 +23,15 @@ import org.eclipse.swt.events.FocusListener;
 public class ViewerSelectionSupport {
 
 	/**
-	 * Create an observable list of the selected item.
+	 * Create an observable list of selected items.
+	 * <p>
+	 * This function create an observable list representing the selection of the
+	 * last focused viewer. This is useful to implement a contextual function.
+	 * i.e.: Every time the focus of the <code>viewers</code> changes, this
+	 * observable will be updated.
 	 * 
 	 * @param viewers
+	 *            the viewer to observed.
 	 */
 	public static IObservableList observeMultiSelectionWithFocus(
 			final Viewer... viewers) {
@@ -54,4 +61,45 @@ public class ViewerSelectionSupport {
 		return ViewerProperties.multipleSelection().observeDetail(
 				lastFocusViewer);
 	}
+
+	/**
+	 * Create an observable value of selected item.
+	 * <p>
+	 * This function create an observable value representing the selection of
+	 * the last focused viewer. This is useful to implement a contextual
+	 * function. i.e.: Every time the focus of the <code>viewers</code> changes,
+	 * this observable will be updated.
+	 * 
+	 * @param viewers
+	 *            the viewer to observed.
+	 */
+	public static IObservableValue observeSingleSelectionWithFocus(
+			final Viewer... viewers) {
+
+		final WritableValue lastFocusViewer = new WritableValue(null,
+				TableViewer.class);
+
+		FocusListener listener = new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				for (Viewer viewer : viewers) {
+					if (viewer.getControl() == e.widget) {
+						lastFocusViewer.setValue(viewer);
+						return;
+					}
+				}
+			}
+		};
+
+		for (Viewer viewer : viewers) {
+			viewer.getControl().addFocusListener(listener);
+			if (viewer.getControl().isFocusControl()) {
+				lastFocusViewer.setValue(viewer);
+			}
+		}
+
+		return ViewerProperties.singleSelection()
+				.observeDetail(lastFocusViewer);
+	}
+
 }
